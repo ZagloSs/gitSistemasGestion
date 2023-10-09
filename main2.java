e760ad6fce3b6943ebef7777e1990c247370d59c
ZagloSs <carlosdiezmendez@gmail.com>
2023-10-09 09:25:35 +0200

    Main prints farewell

--- main2.java
+++ main2.java
@@ -1,5 +1,7 @@
 public static void Main(String[] args){
 	String salutation = "Hola mundo";
 	System.out.println(salutation);
+	String farewell = "chao chao chao chao";
+	System.out.println(farewell);
 
 }

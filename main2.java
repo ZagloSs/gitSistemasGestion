185e5aefe04976ade6602f5881f8e3d288f9bcd3
ZagloSs <carlosdiezmendez@gmail.com>
2023-10-09 10:25:42 +0200

    main now has an adition function

--- main2.java
+++ main2.java
@@ -4,4 +4,12 @@ public static void Main(String[] args){
 	String farewell = "chao chao chao chao";
 	System.out.println(farewell);
 
+	System.out.println(addNumbers(1, 5));
+
+	
+}
+
+public static int  addNumbers(int a, int b){
+
+	return a + b;
 }
